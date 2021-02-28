e6796c29babf80d10c4ac789adb3396b61f80dd3
Penha Vasyl <vasylpenha@gmail.com>
2021-02-28 21:34:40 +0200

    Implement task #2

---
+++ HW-2_JS_Loops_and_Conditions-Penha_Vasyl/index.js
@@ -0,0 +1,28 @@
+let userNumberN = null;
+let userNumberM = null;
+let sumNumbers = 0;
+
+do {
+  userNumberN = + (prompt('Please write іnitial number'));
+  userNumberM = + (prompt('Please write final number'));
+
+  if (userNumberN > userNumberM) {
+    alert('The initial number cannot be greater than the final number. Enter the number again.')
+    continue;
+  }
+  if (isNaN(userNumberN) || isNaN(userNumberM)) {
+    alert('Your numbers is not valid, try again')
+  }
+} while (isNaN(userNumberN) || isNaN(userNumberM))
+
+const integerNumberN = parseInt(userNumberN);
+const integerNumberM = parseInt(userNumberM);
+const skipEvenNumbers = confirm('Whether to skip even numbers?');
+
+for (let i = integerNumberN; i <= integerNumberM; i++) {
+  if (skipEvenNumbers === false || (i % 2)) {
+    sumNumbers += i;
+  }
+}
+
+alert(sumNumbers);
