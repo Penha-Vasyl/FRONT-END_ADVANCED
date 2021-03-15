cda03123f5e7dbd07aaa33c4728bee7f3d4541c1
Penha Vasyl <vasylpenha@gmail.com>
2021-03-15 18:02:26 +0200

    Add a description that is displayed in the console.log

--- HW-7_Closures_A_function's_this_keyword/index.js
+++ HW-7_Closures_A_function's_this_keyword/index.js
@@ -8,7 +8,7 @@ function getMyTaxes(salary) {
   return +(salary * this.tax).toFixed(2);
 }
 
-console.log(getMyTaxes.call(ukraine, 2500));
+console.log(`Кількість податків які сплачуєте в обраній країна: ${getMyTaxes.call(ukraine, 2500)}`);
 
 // 2.Створіть функцію getMiddleTaxes.call(country) -> number; – яка рахує скільки у середньому податків платятт IT-спеціалісти у кожній 
 // країні. (tax * middleSalary). Функція повинна викликатись через call та працювати з даними через this
@@ -16,7 +16,7 @@ function getMiddleTaxes() {
   return +(this.middleSalary * this.tax).toFixed(2);
 }
 
-console.log(getMiddleTaxes.call(litva));
+console.log(`Середня кількість податків які платять IT-спеціалісти в обраній країні: ${getMiddleTaxes.call(litva)}`);
 
 // 3.Створіть функцію getTotalTaxes.call(country) -> number; – яка рахує, скільки всього податків платять IT-спеціалісти у кожній країні. 
 // (tax * middleSalary * vacancies). Функція повинна викликатись через call та працювати з даними через this
@@ -24,7 +24,7 @@ function getTotalTaxes() {
   return +(this.middleSalary * this.tax * this.vacancies).toFixed(2);
 }
 
-console.log(getTotalTaxes.call(latvia));
+console.log(`Всього податків які платять IT-спеціалісти і обраній країні: ${getTotalTaxes.call(latvia)}`);
 
 // 4.Створіть функцію getMySalary(country) – яка буде писати в консоль об'єкт виду: { salary: number, taxes: number, profit: number } 
 // кожні 10 секунд. Значення salary – генеруйте випадковим чином у діапазоні 1500-2000. taxes – розраховується в залежності від вибраної 
